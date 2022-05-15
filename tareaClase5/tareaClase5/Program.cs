726897cc8f13a8d3b3a77ce7ec4dcbf74c5f816b
Agustin <agustinsbarbatty@gmail.com>
2022-05-15 16:02:46 -0300

    App saludar con condicional y bucle

---
+++ tareaClase5/tareaClase5/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string respuesta = "S";
+
+
+while (respuesta.ToLower() == "s")
+{
+    Console.Clear();
+    Console.WriteLine("ingrese su nombre:");
+    var name = Console.ReadLine();
+    Console.WriteLine($"¡Hola {name}!");
+    Console.WriteLine("para continuar precione (S), para finalizar presione (N).");
+    respuesta = Console.ReadLine();
+}
+if (respuesta.ToLower() == "n")
+{
+    Console.WriteLine("Programa finalizado correctamente.");
+}
+else
+{
+    Console.WriteLine("Opcion no valida.");
+}
